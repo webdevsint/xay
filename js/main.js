a864e711af49e5515062452aca12df56adf3e3e7
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2022-04-01 14:25:06 +0600

    latest commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,136 @@
-dragElement(document.getElementById("win1"));
-dragElement(document.getElementById("win2"));
-dragElement(document.getElementById("win3"));
-dragElement(document.getElementById("win4"));
+const desktop = document.querySelector(".desktop");
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+let height = screen.height / 2;
+let width = screen.width / 2;
+
+console.log(screen.width);
+
+if (screen.width < 600) {
+  width = screen.width / 1.3;
+}
+
+function musicWindow() {
+  if (!desktop.classList.contains("musicWindow")) {
+    new WinBox("my music", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      html: '<iframe src="./views/music.html" style="background: #ffff url(./assets/images/folder.png) center center no-repeat;"></iframe>',
+      onclose: function (force) {
+        desktop.classList.remove("musicWindow");
+      },
+    });
+    desktop.classList.add("musicWindow");
   } else {
-    elmnt.onmousedown = dragMouseDown;
+    return;
   }
+}
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
+function beatsWindow() {
+  if (!desktop.classList.contains("beatsWindow")) {
+    new WinBox("beats", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      html: '<iframe src="./views/beats.html" style="background: #ffff url(./assets/images/folder.png) center center no-repeat;"></iframe>',
+      onclose: function (force) {
+        desktop.classList.remove("beatsWindow");
+      },
+    });
+    desktop.classList.add("beatsWindow");
+  } else {
+    return;
   }
+}
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+function drumsWindow() {
+  if (!desktop.classList.contains("drumsWindow")) {
+    new WinBox("drum kits", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      html: '<iframe src="./views/drumkits.html" style="background: #ffff url(./assets/images/folder.png) center center no-repeat;"></iframe>',
+      onclose: function (force) {
+        desktop.classList.remove("drumsWindow");
+      },
+    });
+    desktop.classList.add("drumsWindow");
+  } else {
+    return;
   }
+}
 
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
+function twitterWindow() {
+  if (!desktop.classList.contains("twitterWindow")) {
+    new WinBox("xaylovesnishi - twitter", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      onclose: function (force) {
+        desktop.classList.remove("twitterWindow");
+      },
+      html: '<iframe src="./views/twitter.html" style="background: #1d9bf0 url(./assets/images/twitter.png) center center no-repeat;"></iframe>',
+    });
+    desktop.classList.add("twitterWindow");
+  } else {
+    return;
+  }
+}
+
+function furryWindow() {
+  if (!desktop.classList.contains("furryWindow")) {
+    new WinBox("furry porn", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      html: '<iframe src="./views/furry.html" style="background: #ffff url(./assets/images/folder.png) center center no-repeat;"></iframe>',
+      onclose: function (force) {
+        desktop.classList.remove("furryWindow");
+      },
+    });
+    desktop.classList.add("furryWindow");
+  } else {
+    return;
+  }
+}
+
+function linksWindow() {
+  if (!desktop.classList.contains("linksWindow")) {
+    new WinBox("my links", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      html: '<iframe src="./views/links.html" style="background: #ffff url(https://assets.production.linktr.ee/profiles/_next/static/images/logo-gofundme-green-06c2c752e12167fa4bc44511d7b15de6.gif) center center no-repeat;"></iframe>',
+      onclose: function (force) {
+        desktop.classList.remove("linksWindow");
+      },
+    });
+    desktop.classList.add("linksWindow");
+  } else {
+    return;
+  }
+}
+
+function spotifyWindow() {
+  if (!desktop.classList.contains("spotifyWindow")) {
+    new WinBox("xaydex. - spotify", {
+      height: height,
+      width: width,
+      bottom: 6,
+      border: 3,
+      html: '<iframe src="https://open.spotify.com/embed/artist/614tO82lYAo2eKBmLBXaA4?utm_source=generator&theme=0" width="100%" height="100%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" style="background: black url(./assets/images/spotify.png) center center no-repeat;"></iframe>',
+      onclose: function (force) {
+        desktop.classList.remove("spotifyWindow");
+      },
+    });
+    desktop.classList.add("spotifyWindow");
+  } else {
+    return;
   }
 }
